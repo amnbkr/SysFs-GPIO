f54b6fd566d8ad14abb2fcffb9e46e20890e20db
amnbkr <amin-abobakr@elarabygroup.com>
2022-12-06 14:37:43 +0200

    kernel

--- sysfs-gpio-test.c
+++ sysfs-gpio-test.c
@@ -12,85 +12,86 @@ int pressed;
 int pressed_confidenece_level;
 int released_confidence_level;
 enum { YES, NO } e;
-int main(void) {
-  /*Polling */
-  struct pollfd fdset[2];
-  int nfds = 2;
-  int gpio_fd, timeout, rc;
-  char *buf[MAX_BUF];
-  unsigned int gpio;
-  int len;
-  int val;
+int main(void)
+{
+	/*Polling */
+	struct pollfd fdset[2];
+	int nfds = 2;
+	int gpio_fd, timeout, rc;
+	char *buf[MAX_BUF];
+	unsigned int gpio;
+	int len;
+	int val;
 
-  timeout = 300;
+	timeout = 300;
 
-  gpio_init(LED_UP, "out", "none", "0");
-  gpio_init(BUTTON_UP, "in", "both", "0");
+	gpio_init(LED_UP, "out", "none", "0");
+	gpio_init(BUTTON_UP, "in", "both", "0");
 
-  gpio_init(LED_DOWN, "out", "none", "0");
-  gpio_init(BUTTON_DOWN, "in", "both", "0");
+	gpio_init(LED_DOWN, "out", "none", "0");
+	gpio_init(BUTTON_DOWN, "in", "both", "0");
 
-  // poll
-  gpio_fd = gpio_fd_open(BUTTON_UP);
+	// poll
+	gpio_fd = gpio_fd_open(BUTTON_UP);
 
-  while (1) {
-    if ((gpio_read(BUTTON_UP) == 1)) {
-      pressed_confidenece_level++;
-      if (pressed_confidenece_level > 500) {
-        gpio_write(LED_UP, "1");
-        if (pressed == 0) {
-          pressed = 1;
-          printf("UP\n");
-        }
-        pressed_confidenece_level = 0;
-      }
-    } else if (gpio_read(BUTTON_UP) == 0) {
-      released_confidence_level++;
-      if (released_confidence_level > 500) {
-        pressed = 0;
-        gpio_write(LED_UP, "0");
-        released_confidence_level = 0;
-      }
-    }
-  }
-  while (0) {
-    /*Polling*/
-    memset((void *)fdset, 0, sizeof(fdset));
+	while (1) {
+		if ((gpio_read(BUTTON_UP) == 1)) {
+			pressed_confidenece_level++;
+			if (pressed_confidenece_level > 500) {
+				gpio_write(LED_UP, "1");
+				if (pressed == 0) {
+					pressed = 1;
+					printf("UP\n");
+				}
+				pressed_confidenece_level = 0;
+			}
+		} else if (gpio_read(BUTTON_UP) == 0) {
+			released_confidence_level++;
+			if (released_confidence_level > 500) {
+				pressed = 0;
+				gpio_write(LED_UP, "0");
+				released_confidence_level = 0;
+			}
+		}
+	}
+	while (0) {
+		/*Polling*/
+		memset((void *)fdset, 0, sizeof(fdset));
 
-    fdset[0].fd = STDIN_FILENO;
-    fdset[0].events = POLLIN;
+		fdset[0].fd = STDIN_FILENO;
+		fdset[0].events = POLLIN;
 
-    fdset[1].fd = gpio_fd;
-    fdset[1].events = POLLPRI;
+		fdset[1].fd = gpio_fd;
+		fdset[1].events = POLLPRI;
 
-    rc = poll(fdset, nfds, timeout);
+		rc = poll(fdset, nfds, timeout);
 
-    if (rc < 0) {
-      printf("\npoll() failed!\n");
-      return -1;
-    }
+		if (rc < 0) {
+			printf("\npoll() failed!\n");
+			return -1;
+		}
 
-    if (rc == 0) {
-      // printf(".");
-    }
+		if (rc == 0) {
+			// printf(".");
+		}
 
-    if (fdset[1].revents & POLLPRI) {
-      lseek(fdset[1].fd, 0, SEEK_SET);
-      len = read(fdset[1].fd, buf, MAX_BUF);
-      // printf("\npoll() GPIO %d interrupt occurred\n", gpio);
-      // printf("\tread value: '%c'\n", buf[0]);
-      // Control LED_UP
-      if ((gpio_read(BUTTON_UP) == 1)) {
-        gpio_write(LED_UP, "1");
-        if (pressed == 0) {
-          pressed = 1;
-          printf("UP\n");
-        }
-      } else if (gpio_read(BUTTON_UP) == 0) {
-        pressed = 0;
-        gpio_write(LED_UP, "0");
-      }
-    }
-  }
-  return 0;
+		if (fdset[1].revents & POLLPRI) {
+			lseek(fdset[1].fd, 0, SEEK_SET);
+			len = read(fdset[1].fd, buf, MAX_BUF);
+			// printf("\npoll() GPIO %d interrupt occurred\n", gpio);
+			// printf("\tread value: '%c'\n", buf[0]);
+			// Control LED_UP
+			if ((gpio_read(BUTTON_UP) == 1)) {
+				gpio_write(LED_UP, "1");
+				if (pressed == 0) {
+					pressed = 1;
+					printf("UP\n");
+				}
+			} else if (gpio_read(BUTTON_UP) == 0) {
+				pressed = 0;
+				gpio_write(LED_UP, "0");
+			}
+		}
+	}
+	return 0;
 }
--- sysfs-gpio.c
+++ sysfs-gpio.c
@@ -1,120 +1,129 @@
 #include "sysfs-gpio.h"
 
-int gpio_export(char *pin) {
-  int fd;
-  // write export
-  fd = open("/sys/class/gpio/export", O_WRONLY);
-  if (fd == -1) printf("Cannot open the export file\n");
-  write(fd, pin, strlen(pin));
-  // close Export
-  close(fd);
-  return 0;
-}
-int gpio_unexport(char *pin) {
-  int fd;
-
-  fd = open("/sys/class/gpio/unexport", O_WRONLY);
-  if (fd == -1) {
-    printf("Cannot open the unexport file\n");
-  }
-  // Write unxport
-  write(fd, pin, strlen(pin));
-  // close unexport
-  close(fd);
-  return 0;
-}
-int gpio_direction(char *pin, char *direction) {
-  int fd;
-  char str[MAX_BUF];
-
-  sprintf(str, "/sys/class/gpio/gpio%s/direction", pin);
-  fd = open(str, O_WRONLY);
-  if (fd == -1) printf("Cannot open the direction of %s file\n", pin);
-  // Write Direction
-  write(fd, direction, strlen(direction));
-  // Close Direction
-  close(fd);
-  return 0;
-}
-int gpio_set_edge(char *pin, char *edge) 
+int gpio_export(char *pin)
 {
-  int fd;
-  char str[MAX_BUF];
-
-     sprintf(str, "/sys/class/gpio/gpio%s/edge", pin);
-  fd  = open(str, O_WRONLY) ;
-  if (fd == -1) 
-  {
-    printf("Cannot open the edge of LED_UP file\n");
-  }
-  write(fd, edge, strlen(edge));
-  close(fd);
+	int fd;
+	// write export
+	fd = open("/sys/class/gpio/export", O_WRONLY);
+	if (fd == -1)
+		printf("Cannot open the export file\n");
+	write(fd, pin, strlen(pin));
+	// close Export
+	close(fd);
+	return 0;
 }
-int gpio_set_active_low(char *pin, char *pullup) {
-  int fd;
-  char str[MAX_BUF];
+int gpio_unexport(char *pin)
+{
+	int fd;
 
-  sprintf(str, "/sys/class/gpio/gpio%s/active_low", pin);
-  fd = open(str, O_WRONLY);
-  if (fd == -1) {
-    printf("Cannot open the edge of LED_UP file\n"); //hello
-  }
-  
-  write(fd, pullup, strlen(pullup));
-  close(fd);
+	fd = open("/sys/class/gpio/unexport", O_WRONLY);
+	if (fd == -1) {
+		printf("Cannot open the unexport file\n");
+	}
+	// Write unxport
+	write(fd, pin, strlen(pin));
+	// close unexport
+	close(fd);
+	return 0;
 }
-void gpio_init(char *pin, char *direction, char *edge, char *pullup) {
-  // write export
-  gpio_export(pin);
-  // write direction
-  gpio_direction(pin, direction);
-  // set edge
-  gpio_set_edge(pin, edge);
-  // set pullup
-  gpio_set_active_low(pin, pullup);
-}
-int gpio_write(char *pin, char *value) {
-  int fd;
-  char str[MAX_BUF];
+int gpio_direction(char *pin, char *direction)
+{
+	int fd;
+	char str[MAX_BUF];
 
-  sprintf(str, "/sys/class/gpio/gpio%s/value", pin);
-  fd = open(str, O_WRONLY);
-  if (fd == -1) {
-    printf("Cannot open the value of BUTTON_UP file\n");
-  }
-  write(fd, value, 1);
-  close(fd);
-  return fd;
+	sprintf(str, "/sys/class/gpio/gpio%s/direction", pin);
+	fd = open(str, O_WRONLY);
+	if (fd == -1)
+		printf("Cannot open the direction of %s file\n", pin);
+	// Write Direction
+	write(fd, direction, strlen(direction));
+	// Close Direction
+	close(fd);
+	return 0;
 }
-char gpio_read(char *pin) {
-  int fd;
-  char str[MAX_BUF];
-  char value;
-  char ch;
+int gpio_set_edge(char *pin, char *edge)
+{
+	int fd;
+	char str[MAX_BUF];
 
-  sprintf(str, "/sys/class/gpio/gpio%s/value", pin);
-  fd = open(str, O_RDONLY);
-  if (fd == -1) {
-    printf("Cannot open the value of gpio %s value file\n", pin);
-  }
-  read(fd, &ch, 1);
-  if (ch != '0') {
-    value = 1;
-  } else {
-    value = 0;
-  }
-  close(fd);
-  return value;
+	sprintf(str, "/sys/class/gpio/gpio%s/edge", pin);
+	fd = open(str, O_WRONLY);
+	if (fd == -1) {
+		printf("Cannot open the edge of LED_UP file\n");
+	}
+	write(fd, edge, strlen(edge));
+	close(fd);
+}
+int gpio_set_active_low(char *pin, char *pullup)
+{
+	int fd;
+	char str[MAX_BUF];
+
+	sprintf(str, "/sys/class/gpio/gpio%s/active_low", pin);
+	fd = open(str, O_WRONLY);
+	if (fd == -1) {
+		printf("Cannot open the edge of LED_UP file\n"); // hello
+	}
+
+	write(fd, pullup, strlen(pullup));
+	close(fd);
+}
+void gpio_init(char *pin, char *direction, char *edge, char *pullup)
+{
+	// write export
+	gpio_export(pin);
+	// write direction
+	gpio_direction(pin, direction);
+	// set edge
+	gpio_set_edge(pin, edge);
+	// set pullup
+	gpio_set_active_low(pin, pullup);
+}
+int gpio_write(char *pin, char *value)
+{
+	int fd;
+	char str[MAX_BUF];
+
+	sprintf(str, "/sys/class/gpio/gpio%s/value", pin);
+	fd = open(str, O_WRONLY);
+	if (fd == -1) {
+		printf("Cannot open the value of BUTTON_UP file\n");
+	}
+	write(fd, value, 1);
+	close(fd);
+	return fd;
+}
+char gpio_read(char *pin)
+{
+	int fd;
+	char str[MAX_BUF];
+	char value;
+	char ch;
+
+	sprintf(str, "/sys/class/gpio/gpio%s/value", pin);
+	fd = open(str, O_RDONLY);
+	if (fd == -1) {
+		printf("Cannot open the value of gpio %s value file\n", pin);
+	}
+	read(fd, &ch, 1);
+	if (ch != '0') {
+		value = 1;
+	} else {
+		value = 0;
+	}
+	close(fd);
+	return value;
 }
 /*open file*/
-int gpio_fd_open(char *pin) {
-  int fd;
-  char str[MAX_BUF];
+int gpio_fd_open(char *pin)
+{
+	int fd;
+	char str[MAX_BUF];
 
-  sprintf(str, "/sys/class/gpio/gpio%s/value", pin);
-  fd = open(str, O_RDONLY);
-  if (fd == -1) {
-    printf("Cannot open the value of gpio %s value file\n", pin);
-  }
-  return fd;
+	sprintf(str, "/sys/class/gpio/gpio%s/value", pin);
+	fd = open(str, O_RDONLY);
+	if (fd == -1) {
+		printf("Cannot open the value of gpio %s value file\n", pin);
+	}
+	return fd;
 }
